5c05b6a776991116d0da11a1d0d11836e64cb406
Mehmet F Erten <mferten@mfeweb.com>
2017-06-21 12:25:33 -0400

    commit all

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,42 @@
+@stack('logic')
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <!-- Head: meta, title, link, script statements -->
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+        {{-- css files --}}
+        <link rel="stylesheet" href="css/cssStyle.css">
+        {{-- Google Fonts --}}
+        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+        <!-- require_once("phpfile/require/headTags.php") -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="PHP">
+        <meta name="keywords" content="MFE Sites by Mehmet F Erten (PHP witin Laravel)">
+        <meta name="author" content="MFE Mehmet Fatih Erten">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>
+            @yield('title', 'Find the Country')
+        </title>
+
+    </head>
+
+    <body>
+
+        @stack('header')
+
+        @stack('module')
+
+        <footer class="center">
+
+            <span>&copy; {{ date('Y') }} <span class="mfe">M</span>.<span class="mfe">F</span>.<span class="mfe">E</span>. Sites by Mehmet F Erten</span><br/>
+
+        </footer>
+
+        @stack('jsFiles')
+
+    </body>
+
+</html>
---
+++ resources/views/myWeb/frontPage.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.master')
+
+@push('header')
+    <header>
+        <br/>
+        <h1><span class="mfe">M</span>.<span class="mfe">F</span>.<span class="mfe">E</span> Sites</h1>
+        <h2><span class="mfe">M</span>ehmet <span class="mfe">F</span>atih <span class="mfe">E</span>rten <span class="mfe">M</span>.<span class="mfe">F</span>.<span class="mfe">E</span> Sites</h2>
+    </header>
+@endpush
+
+@push('module')
+    {{-- Beginning of the Flag Selection; --}}
+    <div id="rowID" class="row loginBP">
+        <form id="mfeWeb" class="signInMargin displayFlag" method="post">
+            <br/><br/><br/>
+            <ul>
+                <li>
+                    <a class= "resetAnchor appLanguageSel" href="http://www.mfeworld.com/" target="_blank">Flags or Birds or Flowers or Foods</a>
+                </li>
+                <br/>
+                <li>
+                    <a class= "resetAnchor appLanguageSel" href="http://www.mywebbench.com/" target="_blank">My (your) Web Bench and Virtual Community</a>
+                </li>
+                <br/>
+                <li>
+                    <a class= "resetAnchor appLanguageSel" href="http://www.mfework.com/" target="_blank">One of a kind of a Resort or a Villa</a>
+                </li>
+                <br/>
+                <li>
+                    <a class= "resetAnchor appLanguageSel" href="http://www.mfeweb.com/mail" target="_blank">Work With MFE Mail</a>
+                </li>
+                <br/>
+                <li>
+                    <a class= "resetAnchor appLanguageSel" href="https://github.com/mferten" target="_blank">MFE GitHub</a>
+                </li>
+            </ul>
+            <br/><br/><br/>
+            <div id="flags">
+                <img id="flag21" class="plainFlags vertialAlignMiddle"
+                    src="images/smallFlags/UnitedStatesofAmerica.png"
+                    alt="United States of America">
+                <img id="flag21" class="plainFlags vertialAlignMiddle"
+                    src="images/smallFlags/Turkey.png"
+                    alt="Turkey">
+                <img id="flag21" class="plainFlags vertialAlignMiddle"
+                    src="images/smallFlags/Slovakia.png"
+                    alt="Slovakia">
+            </div>
+        </form>
+    </div>
+@endpush
+
+@push('jsFiles')
+
+@endpush
