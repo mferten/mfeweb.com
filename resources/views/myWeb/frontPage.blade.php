7b83c3addf6c7e7a5433fc97c711bf62806c9168
Mehmet F Erten <mferten@mfeweb.com>
2017-08-29 16:06:29 -0400

    commit all

--- resources/views/myWeb/frontPage.blade.php
+++ resources/views/myWeb/frontPage.blade.php
@@ -3,57 +3,62 @@
 @push('header')
         {{-- Beginning of the Header section --}}
         <header>
-            <br/><br/>
+            <br/><br/> <!-- make this a margin ! -->
             <h1>M.F.E Web Sites <span class="mfeOrange">In progress</span>
-                <br/>
-                <img class="plainFlags vertialAlignMiddle center mfeSmall"
-                    src="images/smallFlags/UnitedStatesofAmerica.png"
-                    alt="United States of America">
-                <img class="plainFlags vertialAlignMiddle center mfeSmall"
-                    src="images/smallFlags/Turkey.png"
-                    alt="Turkey">
-                <img class="plainFlags vertialAlignMiddle center mfeSmall"
-                    src="images/smallFlags/Slovakia.png"
-                    alt="Slovakia">
+                <section class="inputTopMarginFlags">
+                    <img class="plainFlags vertialAlignMiddle center mfeSmall"
+                        src="images/smallFlags/UnitedStatesofAmerica.png"
+                        alt="United States of America">
+                    <img class="plainFlags vertialAlignMiddle center mfeSmall"
+                        src="images/smallFlags/Turkey.png"
+                        alt="Turkey">
+                    <img class="plainFlags vertialAlignMiddle center mfeSmall"
+                        src="images/smallFlags/Slovakia.png"
+                        alt="Slovakia">
+                </section>
             </h1>
         </header>
 @endpush
 
 @push('module')
         {{-- Beginning of the My Sites Link List --}}
-        <div class="row loginBP">
-            <div class="frontPageMarginLeft">
-                <br/>
-                <ul class="ulListStyle">
-                    <li>
-                        <a href="http://www.mfeworld.com/" target="_blank">Flags [Dogs:Birds:Flowers]
-                            <span class="mfe">*1</span></a>
-                    </li>
-                    <li>
-                        <a href="http://www.mywebbench.com/" target="_blank">My Web Bench
-                            <span class="mfe">*1</span></a>
-                    </li>
-                    <li class="marginLiBottom">
-                        <a href="http://www.mfework.com/" target="_blank">My Resorts & Villas
-                            <span class="mfeOrange">*2</span></a>
-                    </li>
-                    <li>
-                        <a href="http://www.mfeweb.com/mail" target="_blank">MFE Mail</a>
-                    </li>
-                    <li class="marginLiBottom">
-                        <a href="https://github.com/mferten" target="_blank">MFE GitHub</a>
-                    </li>
-                    <li>
-                        <span class="mfe">*1 </span><span class="mfeMedium">-Laravel/Elequent(ORM)/Ajax</span><br/>
-                        <span class="mfeSmall">Within CentOS 7 Linux, Apache Web and iRedMail Mail Servers</span>
-                    </li>
-                    <li>
-                        <span class="mfeOrange">*2 </span><span class="mfeMedium">-JHipster/Spring MVC/Hibernate/JPA2(ORM)/Ajax</span><br/>
-                        <span class="mfeSmall">Under Construction</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <section class="row loginBP">
+            <article class="row loginBP">
+                <nav class="frontPageMarginLeft">
+                    <ul class="inputTopMargin ulListStyle">
+                        <li>
+                            <a href="http://www.mfeworld.com/" target="_blank">Flags [Dogs:Birds:Flowers]
+                                <span class="mfe">*1</span></a>
+                        </li>
+                        <li>
+                            <a href="http://www.mywebbench.com/" target="_blank">My Web Bench
+                                <span class="mfe">*1</span></a>
+                        </li>
+                        <li class="marginLiBottom">
+                            <a href="http://www.mfework.com/" target="_blank">My Resorts & Villas
+                                <span class="mfeOrange">*2</span></a>
+                        </li>
+                        <li>
+                            <a href="http://www.mfeweb.com/mail" target="_blank">MFE Mail</a>
+                        </li>
+                        <li class="marginLiBottom">
+                            <a href="https://github.com/mferten" target="_blank">MFE GitHub</a>
+                        </li>
+                    </ul>
+                </nav>
+            </article>
+            <article>
+                <div class="inputTopMargin">
+                <p class="floatingInput"><span class="mfe">*1&nbsp</span>
+                    <span class="mfeMedium">-Laravel/Elequent(ORM)/Ajax</span>
+                    <span class="mfeSmall">Within CentOS 7 Linux, Apache Web and iRedMail Mail Servers</span>
+                </p>
+                <p class="floatingRightInput"><span class="mfeOrange">*2&nbsp</span>
+                    <span class="mfeMedium">-JHipster/Spring MVC/Hibernate/JPA2(ORM)/Ajax</span>
+                    <span class="mfeSmall">Under Construction</span>
+                </p></div>
+            </article>
+        </section>
 @endpush
 
 @push('jsFiles')
