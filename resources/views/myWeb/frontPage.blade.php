a68282722fb0cd548d4743b46c1324fe5d0a9bf2
Mehmet F Erten <mferten@mfeweb.com>
2017-12-21 08:09:28 -0500

    commit all

--- resources/views/myWeb/frontPage.blade.php
+++ resources/views/myWeb/frontPage.blade.php
@@ -20,7 +20,8 @@
                             <span class="superScript mfeSmall">*2</span></a>
                     </li>
                     <li>
-                        <a href="http://www.mywebbench.com" target="_blank">~ My Web Bench</a>
+                        <a href="http://www.mywebbench.com" target="_blank">- My Web Bench
+                            <span class="superScript mfeSmall">*3</span></a>
                     </li>
                     <li>
                         <a href="http://www.mfeworld.com/citations" target="_blank">~ MFE Citations</a>
@@ -41,6 +42,9 @@
                 <p><span class="superScript mfeSmall">*2&nbsp;</span>
                     <span class="mfeMedium">-Spring MVC/Hibernate/JPA2(ORM)/Ajax</span>
                     <span class="mfeSmall">Under Construction</span>
+                </p>
+                <p><span class="superScript mfeSmall">*3&nbsp;</span>
+                    <span class="mfeMedium">-Sandbox: HTML CSS JS</span>
                 </p>
             </article>
         </main>
