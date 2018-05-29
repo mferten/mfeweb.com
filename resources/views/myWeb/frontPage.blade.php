659697c8abfae1a4ac78999df5ac03b2ca715072
Mehmet F Erten <mferten@mfeweb.com>
2018-05-29 12:31:23 -0400

    commit all

--- resources/views/myWeb/frontPage.blade.php
+++ resources/views/myWeb/frontPage.blade.php
@@ -7,7 +7,7 @@
                 <figcaption>
                     <p style='color:#f5b4b4'>Harvard FAS DCE HES (*2019*) ALM DMD</p>
                 </figcaption>
-                <a href="images/programmingcertificate.png" target="_blank" download>
+                <a href="images/programmingcertificate.png" target="_blank">
                     <img class="img14 aPng" src="images/programmingcertificate.png"
                         alt="MFE Harvard Digital Media Design ALM"/>
                 </a>
